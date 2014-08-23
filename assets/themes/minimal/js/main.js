526fc7258215eb020d49f7c47c4dff8276d1e179
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2014-08-23 14:11:03 +0800

    Add package filter front-end user interface. Pull data structure out from logic for easier maintenance.

--- assets/themes/minimal/js/main.js
+++ assets/themes/minimal/js/main.js
@@ -98,4 +98,30 @@ $(document).ready(function() {
     });
     if ($documentSwitcher.data('new')) $documentSwitcher.addClass('new-page');
   }
+  else {
+    // Show/hide binary packages based on package-filter's criteria
+    $('.package-filter .criteria').change(function() {
+      // There could be more than one package-filter in a page, so find the actual one containing the checkbox that received the change event
+      var $packageFilter = $(this).parents('.package-filter');
+      $packageFilter.data('criterias')[this.name] = this.checked; // Update the filter criteria
+      // Then find the package grid which should be located next to the changed package-filter
+      var data = $packageFilter.data('criterias');
+      $packageFilter.next()
+        .find('.binary-package')
+          // Perform the actual filtering
+          .find('.64-bit.STATIC').toggleClass('hidden', !(data['64-bit'] && data.STATIC)).end()
+          .find('.64-bit.SHARED').toggleClass('hidden', !(data['64-bit'] && data.SHARED)).end()
+          .find('.32-bit.STATIC').toggleClass('hidden', !(data['32-bit'] && data.STATIC)).end()
+          .find('.32-bit.SHARED').toggleClass('hidden', !(data['32-bit'] && data.SHARED)).end()
+          .each(function() {
+            // Hide list header when the list itself is hidden
+            var $this = $(this);
+            $this.prev().toggleClass('hidden', !$this.find('li:not(.hidden)').length);
+          }).end()
+        .masonry(); // Relayout the package grid to minimize unused space
+    }).each(function() {
+      // Ensure package-filter's criterias agree with their default values on page reload
+      this.checked = this.defaultChecked;
+    });
+  }
 });
